0e5cb58432b1ea0ac4e948fa473c403d0ce6c816
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-03-28 19:40:09 +0200

    Added ciphers (0, 1, 2, 3, 4, 5, 6, 7, 8, 9). Added All-in-one file. Added cipher 4.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+/*
+
+FCAI - Programming 1 - 2022 - Assignment 2
+Program Name: MorseCode_20210488.cpp
+Program Description: This program includes the Ten ciphers from Assignment #2.
+ Each can cipher and decipher messages.
+
+Last Modification Date: March 28, 2022
+Version: v3.8.0
+Author and contributors: Youssef Moataz, Aya Ali, Hend Ahmed
+ID: 20210488, 20210083, 20211111
+Group: A
+Labs: S4, S4, S9
+Purpose: Cipher and Decipher messages
+
+*/
+
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -353,7 +370,55 @@ void cipherC3() {
 
 // Start Cipher 4
 
+void cipherC4(){
 
+    map<char,string> letters{{'A',"aaaaa"},{'B',"aaaab"},{'C',"aaaba"},{'D',"aaabb"}
+            ,{'E',"aabaa"},{'F',"aabab"},{'G',"aabba"},{'H',"aabbb"},{'I',"abaaa"}
+            ,{'J',"abaab"},{'K',"ababa"},{'L',"ababb"},{'M',"abbaa"},{'N',"abbab"},
+                             {'O',"abbba"},{'P',"abbbb"},{'Q',"baaaa"},{'R',"baaab"},{'S',"baaba"},
+                             {'T',"baabb"},{'U',"babaa"},{'V',"babab"},{'W',"babba"},{'X',"babbb"},
+                             {'Y',"bbaaa"},{'Z',"bbaab"}};
+    int choice;
+    while(choice !=3){
+        cout<<"-If you want to cipher a message enter 1."<<endl<<"-If you want to decipher a message enter 2."<<endl<<"-If you want to exit the program enter 3.";
+        cout<<endl<<"choice :";
+        cin>>choice;
+        string Str,Cipher="",Decipher="",y="",x="";
+        if(choice==1){
+            cout<<"Enter the message you want to cipher :"<<endl;
+            getline(cin>>ws,Str,'\n');
+            for(int i=0;i<Str.length();i++){
+                if(Str[i]==' '){
+                    Str.erase(i,1);
+                    i--;
+                }
+                Str[i]= toupper(Str[i]);
+            }
+            for(char i : Str){
+                Cipher+=letters[toupper(i)];
+                Cipher+=' ';
+            }
+            cout<<Cipher<<endl;}
+        else if(choice==2){
+            cout<<"Enter the message you want to decipher :"<<endl;
+            getline(cin>>ws,Str,'\n');
+            for(int i=0;i<Str.length();i++){
+                if(Str[i]==' '){
+                    Str.erase(i,1);
+                    i--;
+                }}
+            for(int i=0;i<Str.length();i=i+5){
+                for(int j=i;j<5+i;j++){
+                    x+=Str[j];}
+                for(auto element : letters){
+                    if(element.second==x){
+                        Decipher+=element.first;
+                    }
+                }
+                x="";
+            }cout<<Decipher<<endl;}}
+
+}
 
 // End Cipher 4
 
@@ -1359,7 +1424,7 @@ void getMainUserChoice(){
 
         case 4:
 
-            // add c4
+            cipherC4();
 
             break;
 
@@ -1411,3 +1476,5 @@ int main() {
 
     return 0;
 }
+
+// End of the file
